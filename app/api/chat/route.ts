741a4776cffab0b2d645c0dcdc1001226dcff5c9
Amit Kumar Mishra <amitmishrag2020@gmail.com>
2026-04-20 13:28:21 +0530

    3rd

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -5,8 +5,8 @@ export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
 
   const result = streamText({
-    // model: google("gemini-2.0-flash"),
-    model: google("gemini-1.5-flash"),
+    model: google("gemini-2.0-flash"),
+    
     messages:await convertToModelMessages(messages),
   });
 
