6622a19f5a19b94b4c579502794a23f314dfed23
Amit Kumar Mishra <amitmishrag2020@gmail.com>
2026-04-20 14:12:25 +0530

    4done and ai reply

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,14 +1,54 @@
-import { streamText, UIMessage, convertToModelMessages } from "ai";
-import { google } from "@ai-sdk/google";
+// import { streamText, UIMessage, convertToModelMessages } from "ai";
+// import { google } from "@ai-sdk/google";
+
+// export async function POST(req: Request) {
+//   const { messages }: { messages: UIMessage[] } = await req.json();
+
+//   const result = streamText({
+//     model: google("gemini-2.0-flash"),
+    
+//     messages:await convertToModelMessages(messages),
+//   });
+
+//   return result.toUIMessageStreamResponse(); 
+// }
+
+
+
+
+
+
+// import { streamText, UIMessage, convertToModelMessages } from 'ai';
+// import { groq } from "@ai-sdk/groq";
+
+// export async function POST(req: Request) {
+//   const { messages }: { messages: UIMessage[] } = await req.json();
+
+//   const result = streamText({
+//     model: groq("llama3-8b-8192"),
+//     messages: await convertToModelMessages(messages),
+//   });
+
+//   return result.toUIMessageStreamResponse();
+// }
+
+
+
+
+
+
+
+
+import { streamText, UIMessage, convertToModelMessages } from 'ai';
+import { groq } from '@ai-sdk/groq';
 
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
 
   const result = streamText({
-    model: google("gemini-2.0-flash"),
-    
-    messages:await convertToModelMessages(messages),
+    model: groq("llama-3.3-70b-versatile"), // ✅ working model
+    messages: await convertToModelMessages(messages),
   });
 
-  return result.toUIMessageStreamResponse(); 
+  return result.toUIMessageStreamResponse();
 }
